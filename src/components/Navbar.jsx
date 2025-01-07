8007e4ef8c3abfb577d49187c7ba54e12b76cfa4
Pooja675 <poojasinha675@gmail.com>
2025-01-07 18:43:35 +0530

    Redirected to login if unauthenticated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-
-  const user = useSelector((store) => store.user)
+  const user = useSelector((store) => store.user);
   console.log(user);
   return (
     <div className="navbar bg-base-300">
-        <div className="flex-1">
-          <a className="btn btn-ghost text-xl">👩‍💻 DevTinder</a>
-        </div>
-        {user && (
-          <div className="flex-none gap-2">
-          <div className="form-control"> Welcome, {user.firstName}
-          </div>
+      <div className="flex-1">
+        <Link to="/" className="btn btn-ghost text-xl">
+          👩‍💻 DevTinder
+        </Link>
+      </div>
+      {user && (
+        <div className="flex-none gap-2">
+          <div className="form-control"> Welcome, {user.firstName}</div>
           <div className="dropdown dropdown-end mx-5 flex">
             <div
               tabIndex={0}
@@ -21,10 +22,7 @@ const Navbar = () => {
               className="btn btn-ghost btn-circle avatar"
             >
               <div className="w-10 rounded-full">
-                <img
-                  alt="user photo"
-                  src={user.photoUrl}
-                />
+                <img alt="user photo" src={user.photoUrl} />
               </div>
             </div>
             <ul
@@ -32,10 +30,10 @@ const Navbar = () => {
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
             >
               <li>
-                <a className="justify-between">
+                <Link to="/profile" className="justify-between">
                   Profile
                   <span className="badge">New</span>
-                </a>
+                </Link>
               </li>
               <li>
                 <a>Settings</a>
@@ -46,10 +44,9 @@ const Navbar = () => {
             </ul>
           </div>
         </div>
-        )}
-        
-      </div>
-  )
-}
+      )}
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
